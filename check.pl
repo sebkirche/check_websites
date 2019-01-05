8e86e9d5b41106c4ecb0c7327dd87826e2eb0422
Sébastien Kirche <sebastien.kirche@free.fr>
2019-01-05 15:07:56 +0100

    refactored code, added Getopt and Pod help

--- check.pl
+++ check.pl
@@ -2,30 +2,60 @@
 use strict;
 use warnings;
 use feature 'say';
-
 use utf8;
+
+use Cwd qw( cwd );
 use Data::Dumper;
 use Data::Printer;
 use Digest::MD5 'md5_hex';
 use Encode;
+use File::Basename;
 use FindBin qw( $Bin ); # use to know the running dir $Bin is built-in variable
-use LWP::UserAgent;
-use MIME::Lite;
-use POSIX qw( strftime );
+use Getopt::Long;
 use HTML::TreeBuilder::XPath;
+use LWP::UserAgent;
+use Net::SMTP;
+use Pod::Usage;
+use POSIX qw( strftime );
+use Sys::Hostname;
+
+
+my ($arg_hlp, $arg_man, $arg_debug, $arg_verbose, $arg_list) = (0,0,0,0,0);
+GetOptions(
+    'help|h|?'  => \$arg_hlp,
+    'man'       => \$arg_man,
+    'debug|d'   => \$arg_debug,
+    'list|l'    => \$arg_list,
+    'verbose|v' => \$arg_verbose,
+    ) or pod2usage(2);
+pod2usage(1) if $arg_hlp;
+pod2usage(-exitval => 0, -verbose => 2) if $arg_man;
+
+# debug implies verbose
+$arg_verbose = 1 if $arg_debug;
 
 my $cfg_file = "$Bin/check.cfg";
 
-# get the smt authentication from (poor man's) config file
+# Read our poor man's config file
 die "Missing config $cfg_file" unless -e $cfg_file;
 my $cfg = do $cfg_file;
 my $pages        = $cfg->{pages};
 my $persist_file = $cfg->{persist_file};
 my $mail_from    = $cfg->{mail_from};
 my $mail_to      = $cfg->{mail_to};
+my $mail_server  = $cfg->{mail_server};
 my $persist = {};
 
 my $iso_time = '%Y-%m-%dT%H:%M:%SZ';
+my $host = hostname();
+my $whom = getlogin();
+my $path = cwd();
+my $script = basename($0);
+
+if ($arg_list){
+    list_sites();
+    exit 0;
+}
 
 # reload persisted data
 if (-e "$Bin/$persist_file"){
@@ -35,9 +65,13 @@ if (-e "$Bin/$persist_file"){
 }
 # say p $persist;
 
+
+# Define a custom User-Agent
+# - to use the environment-defined proxy
+# - and because some websites refuse to serve Perl ⇒ 406 - Not acceptable
 my $ua = new LWP::UserAgent( 
     env_proxy => 1,
-    agent      => 'Mozilla/4.73 [en] (X11; I; Linux 2.2.16 i686; Nav)', # some websites refuse to serve Perl (406 - Not acceptable)
+    agent      => 'Mozilla/4.73 [en] (X11; I; Linux 2.2.16 i686; Nav)', 
     );
 
 say strftime($iso_time, gmtime time);
@@ -45,14 +79,14 @@ say strftime($iso_time, gmtime time);
 for my $p (@$pages){
     my $name = $p->{name};
     my $url = $p->{url};
-    print "'$name'";
+    print "'$name'" if $arg_verbose;
 
     # get the page
     my $req = new HTTP::Request( GET => $url );
     my $res = $ua->request($req);
     
     my $status = $res->status_line;
-    print " ($status)";
+    print " ($status)" if $arg_debug;
 
     if ($res->is_success){
         my $content;
@@ -73,21 +107,14 @@ for my $p (@$pages){
         if (defined $persist->{$name}{digest}){
             my $old = $persist->{$name}{digest};
             if ($digest eq $old){
-                say " has not changed.";
+                say " has not changed." if $arg_verbose;
             } else {
-                say " HAS CHANGED !!!";
-                my $mime = new MIME::Lite(
-                    From => $mail_from,
-                    To => $mail_to,
-                    Subject => "change detected for $name",
-                    Type    => "text/plain",
-                    Data    => "I am $0\nA change has been detected in the page $url"
-                    );
-                $mime->send() or die "Failed to send mail: $!";
+                say " HAS CHANGED !!!" if $arg_verbose;
+                notify_change($name, $url);
             }
 
         } else {
-            say " was not monitored yet.";
+            say " was not monitored yet." if $arg_verbose;
         }
         $persist->{$name}{digest} = $digest;
     }
@@ -96,10 +123,85 @@ for my $p (@$pages){
     $persist->{$name}{last_check_time} = strftime($iso_time, gmtime time);
 }
 
-open my $p, '>', "$Bin/$persist_file" or die "Cannot open $Bin/$persist_file for save: $!";
+open my $p, '>', "$Bin/$persist_file" or die "Cannot open $Bin/$persist_file for saving states: $!";
 my $dd = new Data::Dumper( [ $persist ] , [ 'persist' ] );
 print $p $dd->Dump;
 close $p;
+
+sub list_sites {
+    for my $site (@$pages){
+        my $detail = "";
+        $detail = "(Full page)" unless exists $site->{xpath};
+        say sprintf "%s - %s %s", $site->{name}, $site->{url}, $detail;
+    }
+}
+
+sub notify_change {
+    my ($name, $url) = @_;
+    
+    my $smtp = Net::SMTP->new($mail_server, $arg_debug ? (Debug => 1) : ());
+    if($smtp){
+        $smtp->mail($mail_from);
+        if($smtp->to(split /,/, $mail_to)){
+            $smtp->data(<<"MSG");
+From: $mail_from
+To: $mail_to
+Subject: change detected for "$name"
+Content-Type: text/plain;
+
+I am $path/$script for $whom\@$host.
+
+A change has been detected in the page of "$name"
+URL is $url
+MSG
+        } else {
+            say STDERR "Failed to send mail: ", $smtp->message();
+        }
+        $smtp->quit();
+    } else {
+        say STDERR "Failed to instanciate Net::SMTP :(";
+    }
+}
+
+__END__
+
+=head1 NAME
+
+  check.pl - An automatic page change tester written in Perl.
+
+=head1 SYNOPSIS
+
+  check.pl [options]
+
+=head1 DESCRIPTION
+
+This is a simple tool that can monitor a list of websites and will send a mail if one had changed.
+
+Typical usage is to be run periodically from a Cron job.
+
+=head1 OPTIONS
+
+=over 4
+
+=item B<-h --help>
+
+  Display a short help.
+
+=item B<--man>
+
+  Display the full manual.
+
+=item B<-v --verbose>
+
+  Show verbose messages during processing.
+
+=item B<-d --debug>
+
+  Activate the debug flag (show SMTP details).
+
+=back
+
+=cut
 
 # Local Variables: 
 # coding: utf-8-unix
